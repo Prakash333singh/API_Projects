d22d43e588453422429b7f20f005156dd947f77d
Prakash singh <pa7846665@gmail.com>
2023-06-13 22:15:11 +0530

    test 1

---
+++ script.js
@@ -0,0 +1,6 @@
+let copyBtn=document.querySelector("#copy-btn");
+let paraDisplay =document.querySelector("#para");
+// let options{
+//     // method:"GET",
+//     headers: {"x-api-key" :apikey}
+// }
